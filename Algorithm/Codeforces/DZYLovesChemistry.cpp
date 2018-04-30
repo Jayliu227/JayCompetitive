fd343687b174f13892e3583a72813bc0912e7576
Jay <jl7895@nyu.edu>
2018-04-30 14:22:47 -0400

    4.29.2018

---
+++ Algorithm/Codeforces/DZYLovesChemistry.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define forn(i,n) for(int i = 0; i < (int)n; i++)
+#define for1(i,n) for(int i = 1; i < (int)n; i++)
+#define FILL(x,v) memset(x,v,sizeof(x))
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+#define endl '\n'
+
+using namespace std;
+
+inline int nxt(){ int x; cin >> x; return x; }
+
+typedef pair<int,int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vii> vvii;
+typedef long long ll;
+
+const int INF = (int) 1e9;
+const int MODULO = (int) 1e9 + 7;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.precision(10);
+#ifdef LOCAL_COMPILATION    
+    int start_s = clock();
+#endif    
+    int n = nxt();
+    int e = nxt();
+    vi adj[n];
+    forn(i, e){
+        int u, v;
+        cin >> u >> v;
+        u--, v--;
+        adj[u].pb(v);
+        adj[v].pb(u);        
+    }
+    int cc = 0;
+    vector<bool> visited(n, false);
+    function<void(int)> dfs = [&](int i){
+        visited[i] = true;
+        for(int j = 0; j < adj[i].size(); j++) if(!visited[adj[i][j]]){
+            dfs(adj[i][j]);
+        }
+    };
+    
+    forn(i, n) if(!visited[i]){
+        cc++;
+        dfs(i);
+    }
+    
+    ll res = 1;
+    for(int i = 0; i < n - cc; i++){
+        res *= 2;
+    }
+    
+    cout << res << endl;
+#ifdef LOCAL_COMPILATION    
+    int stop_s = clock();
+    cerr << "time elapsed: " <<((stop_s - start_s) / double(CLOCKS_PER_SEC)) << "s."<<endl;    
+#endif    
+    return 0;
+}
---
+++ Algorithm/Codeforces/SortingRailwayCars.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define forn(i,n) for(int i = 0; i < (int)n; i++)
+#define for1(i,n) for(int i = 1; i < (int)n; i++)
+#define FILL(x,v) memset(x,v,sizeof(x))
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+#define endl '\n'
+
+using namespace std;
+
+inline int nxt(){ int x; cin >> x; return x; }
+
+typedef pair<int,int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vii> vvii;
+typedef long long ll;
+
+const int INF = (int) 1e9;
+const int MODULO = (int) 1e9 + 7;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.precision(10);
+#ifdef LOCAL_COMPILATION    
+    int start_s = clock();
+#endif    
+    unordered_map<int, int> cnt;
+    int n = nxt();
+    int mx = 0;
+    forn(i, n){
+        int k = nxt();
+        cnt[k] = cnt[k - 1] + 1;
+        mx = max(cnt[k], mx);
+    }    
+    cout << n - mx << endl;
+#ifdef LOCAL_COMPILATION    
+    int stop_s = clock();
+    cerr << "time elapsed: " <<((stop_s - start_s) / double(CLOCKS_PER_SEC)) << "s."<<endl;    
+#endif    
+    return 0;
+}
