d1d5a31007c3c20d74ac5227b36ce53bfb631b7b
Jay <jl7895@nyu.edu>
2018-05-02 19:37:55 -0400

    5.2.2018

---
+++ Algorithm/Codeforces/CountingKangaroosIsFun.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define forn(i,n) for(int i = 0; i < (int)n; i++)
+#define for1(i,n) for(int i = 1; i < (int)n; i++)
+#define FILL(x,v) memset(x,v,sizeof(x))
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+#define endl '\n'
+
+using namespace std;
+
+inline int nxt(){ int x; cin >> x; return x; }
+
+typedef pair<int,int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vii> vvii;
+typedef long long ll;
+
+const int INF = (int) 1e9;
+const int MODULO = (int) 1e9 + 7;
+
+/*
+    key observation: there are at most n/2 pairs
+    so partition them into two parts and do greedy
+*/
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.precision(10);
+#ifdef LOCAL_COMPILATION    
+    int start_s = clock();
+#endif    
+    int n = nxt();
+    vi a(n);
+    forn(i, n) cin >> a[i];        
+    sort(ALL(a));
+    int e = n / 2;
+    int ans = n;
+    for(int i = 0; i < n/2; i++){
+        while(true){
+            if(a[i] * 2 <= a[e]){
+                ans--;
+                e++;
+                break;
+            }else{
+                e++;
+            }
+            if(e == n) break;
+        }
+        if(e == n) break;
+    }        
+    cout << ans << endl;
+#ifdef LOCAL_COMPILATION    
+    int stop_s = clock();
+    cerr << "time elapsed: " <<((stop_s - start_s) / double(CLOCKS_PER_SEC)) << "s."<<endl;    
+#endif    
+    return 0;
+}
