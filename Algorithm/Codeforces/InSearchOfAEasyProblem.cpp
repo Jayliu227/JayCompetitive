801a71ed85ef1036c78fdfc46c8a36bc26880c2e
Jingyu Liu <jingyuliu@10-16-158-255.dynapool.wireless.nyu.edu>
2018-10-01 20:27:21 -0400

    10/1/2018

---
+++ Algorithm/Codeforces/InSearchOfAEasyProblem.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fi first
+#define se second
+#define forn(i,n) for(int i = 0; i < (int)n; i++)
+#define for1(i,n) for(int i = 1; i < (int)n; i++)
+#define FILL(x,v) memset(x,v,sizeof(x))
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+#define endl '\n'
+#define debug(x) cerr << #x << " = " << (x) << ";" << endl
+#define debug2(x, y) cerr << #x << " = " << (x) << "; " << #y << " = " << (y) << ";" << endl
+
+template<typename T> inline bool chkmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<typename T> inline bool chkmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+typedef long long ll;
+
+const int INF = (int) 1e9;
+const int MOD = (int) 1e9 + 7;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.precision(10);
+	
+	int n, a = 0;
+	cin >> n;
+	for(int i = 0; i < n; i++) {
+		int in; cin >> in;
+		a += in;
+	}
+
+	if(a > 0){
+		cout << "HARD" << endl;
+	}else{
+		cout << "EASY" << endl;
+	}
+    return 0;
+}
